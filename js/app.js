e00142af1876b9c65b891f96ba23a63071422dad
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-05-03 23:26:24 -0700

    add documentation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Randomize speed given a min and max
 function getSpeed(min, max) {
     return Math.floor(Math.random() * (max-min))+min;
 }
@@ -5,13 +6,13 @@ function getSpeed(min, max) {
 var Enemy = function(startLane) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.col = 101;
-    this.row = 83;
-    this.rowOffset = 23;
+    this.col = 101; // col width
+    this.row = 83; // row height
+    this.rowOffset = 23; //center sprite in row
     this.sprite = "images/enemy-bug.png";
     this.x = -this.col;
     this.y = (startLane * this.row) - this.rowOffset;
-    this.speed = getSpeed(100, 600);
+    this.speed = getSpeed(100, 600); // set speed min and max
 
 };
 
@@ -20,7 +21,7 @@ var Enemy = function(startLane) {
 Enemy.prototype.update = function(dt) {
     // ensure the game runs at the same speed for all computers.
     this.x += this.speed * dt;
-
+    // when enemy goes offscreen reset to starting position
     if (this.x > 505) {
         this.x = -101;
     };
@@ -32,43 +33,49 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+// Check for collision with Player sprite
 Enemy.prototype.checkCollisions = function() {
-    var playerOffset = 20;
+    var playerOffset = 20; //make collision occur more naturally
+    // create enemy rectangle for collision check
+    // set width/height of collision box
     var enemyRect = {
         x: this.x,
         y: this.y + this.rowOffset,
         width: 100,
         height: 70
     };
+    // create player rectangle for collision check
+    // set width/height of collision box
     var playerRect = {
-        x: player.x + playerOffset,
+        x: player.x+ playerOffset,
         y: player.y + player.rowOffset,
         width: 60,
-        height: 80,
+        height: 80
     };
-
+    //check whether rectangles collide
+    // algorithm used from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
     if (
         enemyRect.x < playerRect.x + playerRect.width &&
         enemyRect.x + enemyRect.width > playerRect.x &&
         enemyRect.y < playerRect.y + playerRect.height &&
         enemyRect.y + enemyRect.height > playerRect.y
     ) {
-        console.log("collision detected");
-        player.reset();
+        player.reset(); //reset player on collision
     };
 };
 
 // Player class
 var Player = function() {
-    this.col = 101;
-    this.row = 83;
-    this.rowOffset = 13
+    this.col = 101; //column width
+    this.row = 83; //row height
+    this.rowOffset = 13 //offset sprite to center in row
+    //default starting position
     this.startPos = {
         row:5,
         col:2
     };
     this.sprite = "images/char-boy.png"
+    //handle movement and limit movement to within grid
     this.handleInput = function(input) {
         switch (input) {
             case 'left':
@@ -78,7 +85,6 @@ var Player = function() {
                 break;
             case 'up':
                 this.y = this.y - this.row;
-                console.log("B");
                 break;
             case 'right':
                 if (this.x < 4 * this.col) {
@@ -92,19 +98,21 @@ var Player = function() {
                 break;
         }
     }
-    this.x = this.startPos.col * this.col;
-    this.y = this.startPos.row * this.row - this.rowOffset;
+    //initialize starting pos
+    this.reset();
 };
 
 // Update the player's position
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
+    // display popup if player reaches water
     if (this.y < this.row - this.rowOffset) {
-      window.alert("CONGRATULATIONS! Level passed!");
-      player.reset();
+        window.alert("CONGRATULATIONS! Level passed!");
+        player.reset();
     };
 };
 
+// Set starting position based on variables in player object
 Player.prototype.reset = function() {
     this.x = this.startPos.col * this.col;
     this.y = this.startPos.row * this.row - this.rowOffset;
@@ -115,12 +123,14 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//initialze enemies
 var enemyA = new Enemy(1);
 var enemyB = new Enemy(2);
 var enemyC = new Enemy(3);
 var enemyD = new Enemy(2);
-
 var allEnemies = [enemyA, enemyB, enemyC, enemyD];
+
+//initialize player
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
